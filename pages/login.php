0db49034bded25dd54416d3027bcccdc39a0011a
Quoc Huy <quochuy7112004@gmail.com>
2025-01-14 11:17:16 +0700

    #5 - demo

--- pages/login.php
+++ pages/login.php
@@ -1,44 +1,47 @@
 <?php
     include_once ('config.php');?>
-
 <?php
     $thongbao = "";
     if (isset($_POST['submit'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
+        $checktype = $_POST['check'];
         $checkuser = $conn->query("SELECT * FROM users WHERE 
         username = '".$conn->real_escape_string($username)."'")->fetch_array();
         if ($username == "" || $password == ""){
             $thongbao = "Vùi lòng nhập đầy đủ thông tin";
         }
-        else if (empty($checkuser)){
+        else if (empty($checkuser) || $checktype != $checkuser['role_id'] || $checktype != 1){
             $thongbao = "Tên tài khoản không tồn tại";
         }
-        else if (!password_verify($password, $checkuser['password'])){
+        else if ($password != $checkuser['password']){
             $thongbao = "Sai mật khẩu";
         }
         else 
         {
+            
             $_SESSION['username'] = $username;
             setcookie("username", $username, time() + 300, "/");
-            echo '<script> location.href = "home.php"</script>';
+            header("Location:home.php");
         }
-    }
+    } 
 
     // đăng nhập vào tài khoản admin
     if (isset($_POST['submit'])){
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $checkuser = $conn->query("SELECT * FROM admin WHERE 
+        $checktype = $_POST['check'];
+        $checkadmin = $conn->query("SELECT * FROM users WHERE 
         username = '".$conn->real_escape_string($username)."'")->fetch_array();
-        if ($username == $checkuser['username'] && $password == $checkuser['password']){
-            $_SESSION['username'] = $username;
-            setcookie("username", $username, time() + 300, "/");
-            echo '<script> location.href = "admin.php"</script>';
+        if (!empty($checkadmin) && $username == $checkadmin['username'] && $password == $checkadmin['password']
+        && $checktype == $checkadmin['role_id'] && $checktype != 1) {
+            
+            $_SESSION['admin'] = $username;
+            setcookie("admin", $username, time() + 300, "/");
+            header("Location:admin.php");
         }
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -51,7 +54,6 @@
     <!--Java script-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
 </head>
 <body>
 <form method = "POST">
@@ -71,6 +73,17 @@
                     <div class="text-center">
                       <img src="images/logo.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
                         width="200px" alt="profile">
+                    </div>
+                    <div class="mb-3" style = "display: flex; justify-content: center; gap: 20px;">
+                        <label>
+                            <input type="radio" name="check" value="1" checked> Sinh viên
+                        </label>
+                        <label>
+                            <input type="radio" name="check" value="2"> Thủ thư
+                        </label>
+                        <label>
+                            <input type="radio" name="check" value="3"> Giám đốc
+                        </label>
                     </div>
                     <div class="mb-3">
                     <input type="text" class="form-control" id="username" name  = "username" aria-describedby="emailHelp"
@@ -101,4 +114,3 @@
 </form>
 </body>
 </html>
-
